c1ac1f97258ea01f49fedf0bbb335cabeb13b468
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-14 23:33:26 -0700

    [feat]: create todo structure, add functionality for checking off and deleting todos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
-document.querySelector("h1").addEventListener("click", function(){
-  this.classList.add("blue");
+// li 'on click' strike through
+$('li').click(function() {
+  $(this).toggleClass("checked");
+});
+
+// 'click' the X to remove the todo
+$('span').click(function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
 });
