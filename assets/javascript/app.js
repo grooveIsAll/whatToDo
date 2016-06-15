aa02fe728596284faf88c66881fc991d679aabda
Bobby Velasco <robert.m.velasco@gmail.com>
2016-06-15 16:32:08 -0700

    [feat]: add icons for input and trash, add toggle animations for both

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,25 @@
+const $input = $('input[type="text"]');
 // li 'on click' strike through
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
   $(this).toggleClass("checked");
 });
 
 // 'click' the X to remove the todo
-$('span').click(function(event) {
+$('ul').on('click', 'span', function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$input.on('keypress', function(event) {
+  if(event.which === 13) {
+    let todoText = $(this).val();
+    $(this).val('');
+    $('ul').append('<li><span><i class="fa fa-trash-o" aria-hidden="true"></i> </span> ' + todoText + '</ul>');
+  }
+});
+
+$('.fa-plus').on('click', function() {
+  $input.fadeToggle();
 });
